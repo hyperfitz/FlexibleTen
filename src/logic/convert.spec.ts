21581779788dcbd963e1eb53a5e98edd2c1748ad
Wolfgang Meyers <wolfgang@donuts.co>
2017-09-16 14:55:29 -0700

    Add fraction conversion

--- src/logic/convert.spec.ts
+++ src/logic/convert.spec.ts
@@ -4,68 +4,111 @@ import {FlexibleNumber} from "./number";
 import * as convert from "./convert";
 import * as render from "./render";
 
-function flex(wholeDigits: Array<number>, numberBase: number): FlexibleNumber {
+function flex(wholeDigits: Array<number>, fractionDigits: Array<number>, numberBase: number): FlexibleNumber {
   const result: FlexibleNumber = {
     numberBase: numberBase,
     wholeDigits: wholeDigits,
-    fractionDigits: [],
+    fractionDigits: fractionDigits,
   };
   return result;
 }
 
 describe("Number conversion", () => {
-  
+
   it("should convert 9 (base 10) to 21 (base 4)", () => {
-    const base10 = flex([9], 10);
+    const base10 = flex([9], [], 10);
     const base4 = convert.convertNumber(base10, 4);
     expect(render.renderNumber(base4)).to.equal("21");
   });
 
   it("should convert 9 (base 10) to 1001 (base 2)", () => {
-    const base10 = flex([9], 10);
+    const base10 = flex([9], [], 10);
     const base2 = convert.convertNumber(base10, 2);
     expect(render.renderNumber(base2)).to.equal("1001");
   });
 
   it("should convert 10 (base 10) to 22 (base 4)", () => {
-    const base10 = flex([0, 1], 10);
+    const base10 = flex([0, 1], [], 10);
     const base4 = convert.convertNumber(base10, 4);
     expect(render.renderNumber(base4)).to.equal("22");
   });
 
   it("should convert 10 (base 10) to 1010 (base 2)", () => {
-    const base10 = flex([0, 1], 10);
+    const base10 = flex([0, 1], [], 10);
     const base4 = convert.convertNumber(base10, 2);
     expect(render.renderNumber(base4)).to.equal("1010");
   });
 
   it("should convert 32 (base 10) to 100000 (base 2)", () => {
-    const base10 = flex([2, 3], 10);
+    const base10 = flex([2, 3], [], 10);
     const base2 = convert.convertNumber(base10, 2);
     expect(render.renderNumber(base2)).to.equal("100000");
   });
 
   it("should convert 31 (base 10) to 11111 (base 2)", () => {
-    const base10 = flex([1, 3], 10);
+    const base10 = flex([1, 3], [], 10);
     const base2 = convert.convertNumber(base10, 2);
     expect(render.renderNumber(base2)).to.equal("11111");
   });
 
   it("should convert 11111 (base 2) to 31 (base 10)", () => {
-    const base2 = flex([1, 1, 1, 1, 1], 2);
+    const base2 = flex([1, 1, 1, 1, 1], [], 2);
     const base10 = convert.convertNumber(base2, 10);
     expect(render.renderNumber(base10)).to.equal("31");
   });
 
   it("should convert 1001 (base 2) to 9 (base 10)", () => {
-    const base2 = flex([1, 0, 0, 1], 2);
+    const base2 = flex([1, 0, 0, 1], [], 2);
     const base10 = convert.convertNumber(base2, 10);
     expect(render.renderNumber(base10)).to.equal("9");
   });
 
   it("should convert 1010 (base 2) to 10 (base 10)", () => {
-    const base2 = flex([0, 1, 0, 1], 2);
+    const base2 = flex([0, 1, 0, 1], [], 2);
     const base10 = convert.convertNumber(base2, 10);
     expect(render.renderNumber(base10)).to.equal("10");
   });
+
+  it("should convert 0.5 (base 10) to 0.1 (base 2)", () => {
+    const base10 = flex([], [5], 10);
+    const base2 = convert.convertNumber(base10, 2);
+    expect(render.renderNumber(base2)).to.equal("0.1");
+  });
+
+  it("should convert 0.25 (base 10) to 0.01 (base 2)", () => {
+    const base10 = flex([], [5, 2], 10);
+    const base2 = convert.convertNumber(base10, 2);
+    expect(render.renderNumber(base2)).to.equal("0.01");
+  });
+
+  it("should convert f (base 16) to 15 (base 10)", () => {
+    const base16 = flex([15], [], 16);
+    const base10 = convert.convertNumber(base16, 10);
+    expect(render.renderNumber(base10)).to.equal("15");
+  });
+
+  it("should convert 0.8 (base 16) to 0.5 (base 10)", () => {
+    const base16 = flex([], [8], 16);
+    const base10 = convert.convertNumber(base16, 10);
+    expect(render.renderNumber(base10)).to.equal("0.5");
+  });
+
+  it("should convert 0.2 (base 10) to 0.0011001100 (base 2)", () => {
+    const base10 = flex([], [2], 10);
+    const base2 = convert.convertNumber(base10, 2);
+    expect(render.renderNumber(base2)).to.equal("0.00110011");
+  });
+
+  it("should convert 2.5 (base 10) to 10.1 (base 2)", () => {
+    const base10 = flex([2], [5], 10);
+    const base2 = convert.convertNumber(base10, 2);
+    expect(render.renderNumber(base2)).to.equal("10.1");
+  });
 });
+
+/*
+base 10   base 2    fraction
+.5        .1        1/2
+.25       .01       1/4
+.125      .001      1/8
+*/
--- src/logic/convert.ts
+++ src/logic/convert.ts
@@ -1,20 +1,69 @@
 import {FlexibleNumber} from "./number";
 
+const MaxDecimalPlaces = 10;
+
 export function convertNumber(num: FlexibleNumber, toBase: number): FlexibleNumber {
   const result: FlexibleNumber = {
     wholeDigits: [],
     fractionDigits: [],
     numberBase: toBase,
   };
-  let wholeDigits = recursiveConvert(num.wholeDigits[0], toBase);
-  for (let i = 1; i < num.wholeDigits.length; i++) {
-    wholeDigits = addFlexibleNumbers(toBase, wholeDigits, recursiveConvert(num.wholeDigits[i] * Math.pow(num.numberBase, i), toBase));
+  if (num.wholeDigits.length) {
+    result.wholeDigits = convertDigitSet(num.wholeDigits, num.numberBase, toBase);
   }
-  result.wholeDigits = wholeDigits;
+  if (num.fractionDigits.length) {
+    let fractionNumerator = getDigitSetValue(num.fractionDigits, num.numberBase);
+    let fractionDenominator = Math.pow(num.numberBase, num.fractionDigits.length);
+    // Multiply numerator by number base to skip the initial 0 to the left of the decimal point
+    result.fractionDigits = longDivision(fractionNumerator * toBase, fractionDenominator, toBase);
+    trimZeroPadding(result.fractionDigits);
+  }
   return result;
 }
 
-function addFlexibleNumbers(numberBase: number, num1: Array<number>, num2: Array<number>, index: number=0, carry: number=0, result?: Array<number>): Array<number> {
+function trimZeroPadding(digitSet: Array<number>) {
+  let i = digitSet.length;
+  while (digitSet[i - 1] == 0) {
+    i--;
+  }
+  digitSet.splice(i, digitSet.length - i);
+}
+
+function longDivision(numerator: number, denominator: number, numberBase: number, result?: Array<number>): Array<number> {
+  if (result == null) {
+    result = [];
+  }
+  if (result.length < MaxDecimalPlaces) {
+    const quotient = Math.floor(numerator / denominator);
+    result.push(quotient);
+    const remainder = numerator % denominator;
+    // console.log("num=" + numerator + ", den=" + denominator + ", quot=" + quotient + ", rem=" + remainder);
+    if (remainder > 0) {
+      numerator -= quotient * denominator;
+      numerator *= numberBase;
+      longDivision(numerator, denominator, numberBase, result);
+    }
+  }
+  return result;
+}
+
+function getDigitSetValue(digits: Array<number>, numberBase: number): number {
+  let result = 0;
+  digits.forEach((digit, index) => {
+    result += digit * Math.pow(numberBase, index);
+  });
+  return result;
+}
+
+function convertDigitSet(digits: Array<number>, srcBase: number, destBase: number): Array<number> {
+  let result = convertDigit(digits[0], destBase);
+  for (let i = 1; i < digits.length; i++) {
+    result = addDigitSet(destBase, result, convertDigit(digits[i] * Math.pow(srcBase, i), destBase));
+  }
+  return result;
+}
+
+function addDigitSet(numberBase: number, num1: Array<number>, num2: Array<number>, index: number=0, carry: number=0, result?: Array<number>): Array<number> {
   if (result == null) {
     result = [];
   }
@@ -32,11 +81,11 @@ function addFlexibleNumbers(numberBase: number, num1: Array<number>, num2: Array
   result.push(resultDigit);
 
   carry = Math.floor(sum / numberBase);
-  addFlexibleNumbers(numberBase, num1, num2, index + 1, carry, result);
+  addDigitSet(numberBase, num1, num2, index + 1, carry, result);
   return result;
 }
 
-function recursiveConvert(src: number, toBase: number, result: Array<number>=null): Array<number> {
+function convertDigit(src: number, toBase: number, result: Array<number>=null): Array<number> {
   // console.log("convert: num=" + src);
   if (result == null) {
     result = [];
@@ -45,7 +94,7 @@ function recursiveConvert(src: number, toBase: number, result: Array<number>=nul
   result.push(resultDigit);
   const carry = Math.floor(src / toBase);
   if (carry > 0) {
-    recursiveConvert(carry, toBase, result);
+    convertDigit(carry, toBase, result);
   }
   return result;
 }
